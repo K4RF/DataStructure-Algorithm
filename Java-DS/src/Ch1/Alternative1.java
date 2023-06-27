906638c003a8607a93db612e99e7d621c01367cf
K4RF <rpdirwk1025@naver.com>
2023-06-27 21:28:43 +0900

    반복문 실습 2

---
+++ Java-DS/src/Ch1/Alternative1.java
@@ -0,0 +1,28 @@
+package Ch1;
+
+import java.util.Scanner;
+
+public class Alternative1 {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("+와 -를 번갈아 n개 출력합니다.");
+        int n;
+        
+        do{
+            System.out.print("n값: ");
+            n = stdIn.nextInt();
+        }while(n<= 0);
+        
+        int sum = 0;
+        for(int i =0; i<n; i++){
+            if(i % 2 == 0){
+                System.out.print("+");
+            }
+            else{
+                System.out.print("-");
+            }
+        }
+        stdIn.close();
+    }
+}
