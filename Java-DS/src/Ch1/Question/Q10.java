c570d4c3619b3892d313835eba763030215aefb7
K4RF <rpdirwk1025@naver.com>
2023-06-27 21:18:17 +0900

    연습문제 9,10번

---
+++ Java-DS/src/Ch1/Question/Q10.java
@@ -0,0 +1,19 @@
+package Ch1.Question;
+
+import java.util.Scanner;
+
+public class Q10 {
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+
+        System.out.print("양의 정수 입력: ");
+        int n = sc.nextInt();
+        int count = 0;
+        do{
+            n/=10;
+            count++;
+        }while(n!=0);
+        System.out.println("그 수는 " + count+ "자리입니다.");
+        sc.close();
+    }
+}
